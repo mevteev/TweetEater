1237a63eaf2db3827d091509cf8d25e154bfa55b
michal <mevteev@gmail.com>
2018-06-05 00:52:12 +0300

    Initial commit

---
+++ src/main/java/org/jewtiejew/tweeteater/TweeteaterApplication.java
@@ -0,0 +1,12 @@
+package org.jewtiejew.tweeteater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeteaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweeteaterApplication.class, args);
+    }
+}
